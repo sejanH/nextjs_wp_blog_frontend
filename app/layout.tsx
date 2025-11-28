88cf62e7e0545855b645c6cf017e3fdf87f7837f
Mominul Haque <sejan840@gmail.com>
2025-11-28 20:08:37 +0600

    ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const sans = Manrope({
   variable: "--font-body",
@@ -34,6 +35,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${sans.variable} ${display.variable} antialiased`}>
         {children}
+        <Script
+          src="//pl8212799.effectivegatecpm.com/dd/87/e0/dd87e0d92f96702bf7cba66aa4d3d810.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="//pl15724785.effectivegatecpm.com/fa/3d/1e/fa3d1e8c202a6156bf8737806f3e6367.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
